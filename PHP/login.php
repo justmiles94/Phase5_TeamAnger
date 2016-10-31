ca806dde924488f9920367526f15bea424799cac
justmiles94 <justmiles94@gmail.com>
2016-10-31 17:17:21 -0500

    finished login php file

--- PHP/login.php
+++ PHP/login.php
@@ -6,6 +6,9 @@ Allows PHP to connect to your database
 
 include('connectUserDB.php');
 
+$username = $_POST['username'];
+$password = $_POST['password'];
+
 $sql = "select * from Users where username='".$username."' and password='".$password."'";
 if ($connection->query($sql)->num_rows > 0) {
     echo "true";
