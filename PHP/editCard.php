e8900e6092755f5f932c5cd7c703fc964db5cdee
Zachary Auer <zauer@siue.edu>
2016-10-31 13:49:32 -0500

    Updating to Justin's Version

--- PHP/editCard.php
+++ PHP/editCard.php
@@ -1,3 +1,19 @@
 <?php
-	
+/*
+CONNECT-DB.PHP
+Allows PHP to connect to your database
+*/
+
+include('connectDB.php');
+
+$name = $_POST['name'];
+$firstName = $_POST['firstname'];
+
+$sql = "UPDATE Library SET lastname='Doe' WHERE id=2";
+
+if ($connection->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
 ?>
--- PHP/library.php
+++ PHP/library.php
@@ -1,3 +1,16 @@
 <?php
-	
+/*
+CONNECT-DB.PHP
+Allows PHP to connect to your database
+*/
+
+include('connectDB.php');
+
+$sql = "select * from Library order by name asc";
+
+if ($connection->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
 ?>
