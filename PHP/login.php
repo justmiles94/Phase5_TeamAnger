2ee9a2999c7510e5458992439775e82faefe8415
Zachary Auer <zauer@siue.edu>
2016-10-31 19:12:10 -0500

    This is changing the Library Page by adding a bacground to the top and a button to return to the main menu.

--- PHP/login.php
+++ PHP/login.php
@@ -6,8 +6,6 @@ Allows PHP to connect to your database
 
 include('connectUserDB.php');
 
-$username = $_POST['username'];
-$password = $_POST['password'];
 
 $sql = "select * from Users where username='".$username."' and password='".$password."'";
 if ($connection->query($sql)->num_rows > 0) {
