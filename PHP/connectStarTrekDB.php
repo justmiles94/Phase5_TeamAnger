ed8afcd07c747ba89a12cf9ed20e42cf780784a2
justmiles94 <justmiles94@gmail.com>
2016-11-03 03:58:51 -0500

    conncection update

--- PHP/connectStarTrekDB.php
+++ PHP/connectStarTrekDB.php
@@ -11,7 +11,7 @@ $pass = 'Jwad0194';
 $db = 'startrek';
 
 // Connect to Database
-$connection = mysql_connect($server, $user, $pass)
+$connection = new mysqli($server, $user, $pass)
 or die ("Could not connect to server ... \n" . mysql_error ());
 mysql_select_db($db)
 or die ("Could not connect to database ... \n" . mysql_error ());
